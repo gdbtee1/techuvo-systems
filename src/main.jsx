1d804899524858b3a9be7353f186842205e2b984
Techuvo <techuvo.dev@gmail.com>
2026-05-19 23:03:45 -0400

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+  <HashRouter>
+  <App />
+</HashRouter>
   </React.StrictMode>
 );
