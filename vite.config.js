197c9e186b5ff62f1f7a153b446c533f2af9fcda
Techuvo <techuvo.dev@gmail.com>
2026-05-23 20:11:42 -0400

    Commited

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-base: "/",
+  plugins: [react(), tailwindcss()],
+  base: "/", // keep for custom domain BUT must be redeployed correctly
 });
