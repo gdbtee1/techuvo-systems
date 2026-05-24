d8b9db1b958477eb7aca64c11c888c1c97926b76
Techuvo <techuvo.dev@gmail.com>
2026-05-23 20:09:03 -0400

    Back Up

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 import Home from "./pages/Home";
-import Portfolio from "./pages/Portfolio";
+import Shop from "./pages/Shop";
+import Services from "./pages/Services";
 import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <>
+    <div className="min-h-screen bg-white">
       <Navbar />
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/portfolio" element={<Portfolio />} />
       </Routes>
-    </>
+    </div>
   );
 }
