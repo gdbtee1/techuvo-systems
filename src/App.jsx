92836480c270586e40eceaaccbb27ee2c4f48bcc
Techuvo <techuvo.dev@gmail.com>
2026-09-02 16:55:42 -0400

    Configure Care By The Hour deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,17 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import Layout from "./components/layout/Layout";
+import Home from './pages/Home'
 
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Portfolio from "./pages/Portfolio";
-import Pricing from "./pages/Pricing";
-import Contact from "./pages/Contact";
-import WebsiteOffer from "./pages/WebsiteOffer";
-import WebsiteOnboarding from "./pages/WebsiteOnboarding";
-
-import WebsiteDevelopment from "./pages/services/WebsiteDevelopment";
-import LandingPages from "./pages/services/LandingPages";
-import BrandIdentity from "./pages/services/BrandIdentity";
-import AutomationSystems from "./pages/services/AutomationSystems";
-
-import Detroit from "./pages/locations/Detroit";
-import Ypsilanti from "./pages/locations/Ypsilanti";
-import AnnArbor from "./pages/locations/AnnArbor";
-import Dearborn from "./pages/locations/Dearborn";
-import Livonia from "./pages/locations/Livonia";
-import Southfield from "./pages/locations/Southfield";
+import './styles/global.css'
 
 function App() {
   return (
-    <Routes>
-      {/* Dedicated Google Ads landing page */}
-      <Route path="/start" element={<WebsiteOffer />} />
-
-      <Route
-        path="/start/onboarding"
-        element={<WebsiteOnboarding />}
-      />
-
-      {/* Main Techuvo website */}
-      <Route element={<Layout />}>
-        <Route index element={<Home />} />
-
-        <Route path="/about" element={<About />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/contact" element={<Contact />} />
-
-        <Route
-          path="/services/website-development"
-          element={<WebsiteDevelopment />}
-        />
-
-        <Route
-          path="/services/landing-pages"
-          element={<LandingPages />}
-        />
-
-        <Route
-          path="/services/brand-identity"
-          element={<BrandIdentity />}
-        />
-
-        <Route
-          path="/services/automation-systems"
-          element={<AutomationSystems />}
-        />
-
-        <Route path="/locations/detroit" element={<Detroit />} />
-        <Route path="/locations/ypsilanti" element={<Ypsilanti />} />
-        <Route path="/locations/ann-arbor" element={<AnnArbor />} />
-        <Route path="/locations/dearborn" element={<Dearborn />} />
-        <Route path="/locations/livonia" element={<Livonia />} />
-        <Route path="/locations/southfield" element={<Southfield />} />
-
-        <Route path="/services" element={<Navigate to="/" replace />} />
-        <Route path="/locations" element={<Navigate to="/" replace />} />
-
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Route>
-    </Routes>
-  );
+    <BrowserRouter basename="/carebythehour">
+      <Routes>
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
